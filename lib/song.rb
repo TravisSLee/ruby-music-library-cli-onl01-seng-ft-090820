0792c0e46fed5a82ed0d5bf25f45dd82f22efd54
Travis Lee <travisslee223@gmail.com>
2020-10-18 18:50:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,13 +39,13 @@ class Song
     end
   end
   
-  def self.find_by_name(name)
-    self.all.detect{|song| song.name == name}
-  end
+  # def self.find_by_name(name)
+  #   self.all.detect{|song| song.name == name}
+  # end
   
-  def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name)
-  end
+  # def self.find_or_create_by_name(name)
+  #   self.find_by_name(name) || self.create(name)
+  # end
   
   def self.new_from_filename(file)
     song_name = file.split(" - ")[1]
